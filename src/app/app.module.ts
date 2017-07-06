dbc25114fce1edb194ae5db6ce9cce615395e8c9
Juan Carlos Suarez Pelaez <juancarlos.suarez@micrium.com.bo>
2017-07-06 11:46:45 -0400

    creando reouter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
+import { RouterModule }   from '@angular/router';
 
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule  ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  imports:      [ BrowserModule, FormsModule ,
+		RouterModule.forRoot([
+		  	{
+		    	path: 'heroes',
+		    	component: HeroesComponent
+		  	},
+		  	{
+				path: 'dashboard',
+				component: DashboardComponent
+		  	},
+		  	{
+			  path: '',
+			  redirectTo: '/dashboard',
+			  pathMatch: 'full'
+			},
+		]) 
+	],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  providers: 	[ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
